09fcf4fc46416ae2b106e9975f705c44f822e670
masqqg <cr7.eap7@gmail.com>
2026-09-05 18:46:48 +0300

    Update Addon.java

--- src/main/java/com/maza/addon/Addon.java
+++ src/main/java/com/maza/addon/Addon.java
@@ -3,15 +3,35 @@ package com.maza.addon;
 import com.maza.addon.modules.ActivityDebug;
 import com.maza.addon.modules.MovementDebug;
 import com.maza.addon.modules.EntityTracker;
+import com.maza.addon.modules.SpeedMineBypass;
 import meteordevelopment.meteorclient.addons.MeteorAddon;
+import meteordevelopment.meteorclient.systems.modules.Categories;
 import meteordevelopment.meteorclient.systems.modules.Modules;
 
-public class Addon extends MeteorAddon {
+public class MazaAddon extends MeteorAddon {
     @Override
     public void onInitialize() {
+        Categories.add(MazaCategory.INSTANCE);
+        
         Modules.get().add(new ActivityDebug());
         Modules.get().add(new MovementDebug());
         Modules.get().add(new EntityTracker());
+        Modules.get().add(new SpeedMineBypass());
+    }
+
+    @Override
+    public void onRegisterCategories() {
+        Categories.add(MazaCategory.INSTANCE);
+    }
+
+    @Override
+    public String getWebsite() {
+        return null;
+    }
+
+    @Override
+    public String getGithubRepo() {
+        return null;
     }
 
     @Override
